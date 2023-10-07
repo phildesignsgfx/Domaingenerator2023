8fd38971ca5eda843afcba625d738da234ca527b
Philippe <82619578+phildesignsgfx@users.noreply.github.com>
2023-10-07 15:08:09 +0000

    start

--- src/app.js
+++ src/app.js
@@ -5,7 +5,21 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let pronouns = ["the", "our"];
+let adjectives = ["great", "big"];
+let nouns = ["jogger", "racoon"];
+let domain = [".com", ".io", ".net", ".uy"];
+
+const final = () => {
+  let result = pronouns[Math.floor(Math.random() * pronouns.length)];
+  result += adjectives[Math.floor(Math.random() * adjectives.length)];
+  result += nouns[Math.floor(Math.random() * nouns.length)];
+  result += domain[Math.floor(Math.random() * domain.length)];
+  return result;
+};
+
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let finalDomain = document.querySelector("#finalDomain");
+  finalDomain.innerHTML = final();
 };
